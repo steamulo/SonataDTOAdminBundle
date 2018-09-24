1e1e575d12d78c3fe40ada43c5c0722b7dac766f
Aurélien Soulard <aurelien@vtech.fr>
2018-09-24 15:47:19 +0200

    Correction des filtres lors de l'héritage d'admin

--- src/Filter/AbstractFilter.php
+++ src/Filter/AbstractFilter.php
@@ -3,6 +3,7 @@
 namespace Vtech\Bundle\SonataDTOAdminBundle\Filter;
 
 use Sonata\AdminBundle\Filter\Filter;
+use Vtech\Bundle\SonataDTOAdminBundle\Model\ModelManager;
 
 abstract class AbstractFilter extends Filter
 {
@@ -16,6 +17,18 @@ abstract class AbstractFilter extends Filter
             $alias = null;
             if (!empty($this->getParentAssociationMappings())) {
                 $alias = implode('.', $this->getParentAssociationMappings());
+            }
+
+            if ((null !== $modelManager = $this->getFieldOption('model_manager')) && is_object($value['value'])) {
+                $class = $this->getFieldOption('class');
+                if ($modelManager instanceof ModelManager && null !== $class && $value['value'] instanceof $class) {
+                    $value['value'] = $modelManager->getDenormalizedIdentifier(
+                        $class,
+                        $modelManager->getNormalizedIdentifier($value['value'])
+                    );
+
+                    $this->value = $value;
+                }
             }
 
             $this->filter($query, $alias, $this->getFieldName(), $value);
